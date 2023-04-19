e3ff33922679919b4cb5adc004203b328f6e5a28
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 03:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-//complete this code
-class Animal {}
-
-class Dog extends Animal {}
-
-class Cat extends Animal {}
-
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+  get species() {
+    return this._species;
+  }
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
+}
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
